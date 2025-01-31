0c57ab42c7247e9b6ec2049247a007938f2d2008
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 <lynx87@ukr.net>
2025-01-31 15:32:51 +0200

    fix mail

--- codewars_solutions/utils.py
+++ codewars_solutions/utils.py
@@ -1,2 +1,6 @@
 def odd_count(n: int) -> int:
     return n // 2
+
+def is_isogram(string):
+    string = string.lower()#ignor
+    return len(string) == len(set(string))#unique symbol
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,5 +1,5 @@
 import unittest
-from codewars_solutions.utils import odd_count
+from codewars_solutions.utils import odd_count, is_isogram
 
 class TestOddCount(unittest.TestCase):
     def test_odd_count(self):
@@ -7,6 +7,16 @@ class TestOddCount(unittest.TestCase):
         self.assertEqual(odd_count(15), 7)
         self.assertEqual(odd_count(1), 0)
         self.assertEqual(odd_count(100), 50)
+
+class TestIsIsogram(unittest.TestCase):
+    def test_is_isogram(self):
+        self.assertTrue(is_isogram("Dermatoglyphics"))  # Усі символи унікальні
+        self.assertFalse(is_isogram("aba"))  # 'a' повторюється
+        self.assertFalse(is_isogram("moOse"))  # 'o' повторюється (нечутливе до регістру)
+        self.assertTrue(is_isogram(""))  # Порожній рядок - ізограма
+        self.assertTrue(is_isogram("isogram"))  # Усі символи унікальні
+        self.assertFalse(is_isogram("hello"))  # 'l' повторюється
+        self.assertFalse(is_isogram("Alphabet"))  # 'a' повторюється
 
 if __name__ == "__main__":
     unittest.main()
